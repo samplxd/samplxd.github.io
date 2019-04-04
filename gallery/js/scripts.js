b9844e9bf5eb59acb245c07d775142ae4aaed743
samplxd <43620382+samplxd@users.noreply.github.com>
2019-04-05 01:41:46 +0200

    Update scripts.js

--- gallery/js/scripts.js
+++ gallery/js/scripts.js
@@ -1,7 +1,7 @@
-var addimage = function {
+var addimage = function() {
 var ifrm = document.createElement('iframe');
 ifrm.setAttribute('id', 'ifrm');
 var el = document.getElementById('marker');
 el.parentNode.insertBefore(ifrm, el);
-ifrm.setAttribute('src', 'gallery.html');
+ifrm.setAttribute('src', 'https://coolors.co/');
 }
