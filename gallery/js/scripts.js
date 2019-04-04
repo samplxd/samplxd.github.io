a29df057eae3d705c45d6d1f2ad4c107b1aa928b
samplxd <43620382+samplxd@users.noreply.github.com>
2019-04-04 14:52:20 +0200

    Update scripts.js

--- gallery/js/scripts.js
+++ gallery/js/scripts.js
@@ -1,40 +1,5 @@
-	const menuicon = document.querySelector("#menuextend");
-	const menulist = document.querySelector("#menulist");
-	
-	var iconcheck = 0;
-	
-	var photoActionIn = function(){
-        menuicon.classList.add("extended");
-		menulist.classList.remove("inactive");
-    }
-	
-	var photoActionClick = function(){
-        
-		if (iconcheck == 0){
-		menuicon.classList.add("extended");
-		menulist.classList.remove("inactive");
-		iconcheck = 1;
-		}
-		else {
-		menuicon.classList.remove("extended");
-		menulist.classList.add("inactive");
-		iconcheck = 0;
-		}
-    }
-	 
-    var photoActionOut = function(){
-        if (iconcheck == 0){
-		menuicon.classList.remove("extended");
-		menulist.classList.add("inactive");
-		}
-	}
-	
-	menuicon.addEventListener("mouseover", photoActionIn);
-	menuicon.addEventListener("click", photoActionClick);
-	menuicon.addEventListener("mouseout", photoActionOut);
-	
-	menulist.addEventListener("mouseover", photoActionIn);
-	menulist.addEventListener("click", photoActionClick);
-	menulist.addEventListener("mouseout", photoActionOut);
-
-
+var ifrm = document.createElement('iframe');
+ifrm.setAttribute('id', 'ifrm');
+var el = document.getElementById('marker');
+el.parentNode.insertBefore(ifrm, el);
+ifrm.setAttribute('src', 'gallery.html');
