9d84b873be73beecec07b18cd2c95e2c211d17bd
samplxd <43620382+samplxd@users.noreply.github.com>
2019-04-04 14:53:56 +0200

    Update scripts.js

--- gallery/js/scripts.js
+++ gallery/js/scripts.js
@@ -1,5 +1,7 @@
+var addimage = function {
 var ifrm = document.createElement('iframe');
 ifrm.setAttribute('id', 'ifrm');
 var el = document.getElementById('marker');
 el.parentNode.insertBefore(ifrm, el);
 ifrm.setAttribute('src', 'gallery.html');
+}
